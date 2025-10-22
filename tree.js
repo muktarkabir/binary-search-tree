5af1ec48e324f6d60e9343c143b7d597fdfc6850
muktarkabir <mukhtarkabir3394@gmail.com>
2025-10-23 00:56:02 +0100

    Implement buildTree function.

--- tree.js
+++ tree.js
@@ -5,4 +5,19 @@ class Tree {
   }
 }
 
-function buildTree(array) {}
+function buildTree(array) {
+  let sortedArrayToSet = new Set(array.sort((a, b) => a - b));
+  let uniqueArray = [...sortedArrayToSet];
+
+  const sortedArrayToBSTRecur = (arr, start, end) => {
+    if (start > end) return null;
+    let mid = start + Math.floor((end - start) / 2);
+    let root = new Node(arr[mid]);
+    // Divide from middle element
+    root.left = sortedArrayToBSTRecur(arr, start, mid - 1);
+    root.right = sortedArrayToBSTRecur(arr, mid + 1, end);
+    return root;
+  };
+
+  return sortedArrayToBSTRecur(uniqueArray);
+}
